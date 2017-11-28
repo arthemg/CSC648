5111e7993e96e5da96b1d7283539abf344c90f2f
arthemg <arthemg@gmail.com>
2017-11-27 19:26:38 -0800

    login_signup_work

--- nodeApp/modules/users.js
+++ nodeApp/modules/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var dbConnection = require('../config/db');
+var bcrypt = require('bcrypt-nodejs');
 
 var getAllUsers = function (req, res, next) {
     // req.getConnection(function (err, conn) {
@@ -42,7 +43,7 @@ var addNewUser = function (req, res, next)
     var data = {
         name:req.body.name,
         email:req.body.email,
-        password:req.body.password
+        password: bcrypt.hashSync(req.body.password, null, null)
     };
 
     //inserting into mysql
@@ -72,7 +73,7 @@ var getSearchUsersPage = function(req,res) {
 var searchUser = function(req, res, next)
 {
     var user = req.params.user;
-    console.log(user, 'user');
+    // console.log(user, 'user');
 
 
     // req.getConnection(function(err,conn)
@@ -132,7 +133,7 @@ var updateUserInfo = function (req, res, next)
 {
         var user_id = req.params.user_id;
 
-        console.log(req.params, 'User Id in updateUser');
+        // console.log(req.params, 'User Id in updateUser');
 
     //validation
     req.assert('name','Name is required').notEmpty();
@@ -195,6 +196,66 @@ var deleteUser = function (req, res, next) {
      //});
 };
 
+var getSignupForm = function(req, res){
+    res.render('signup',{title:"SignUp Page", message:''});
+};
+
+var registerUser = function (req, res, next)
+{
+    //console.log(req.body,"REQUEST");
+
+    req.assert('first_name','First Name is required').notEmpty();
+    req.assert('last_name','Last Name is required').notEmpty();
+    req.assert('phone','First Name is required', 'Should be numbers Only!').len(10, 20).isNumeric();
+    req.assert('email','Email is required').isEmail();
+    req.assert('password', 'Password is required', 'Must be at least 6 characters long').len(6, 20).notEmpty();
+
+    var errors = req.validationErrors();
+
+    if(errors){
+        res.status(422).json(errors);
+        return;
+    }
+
+    dbConnection.query("SELECT * FROM t_user WHERE email =?", [req.body.email], function(err, rows){
+        if(err){
+            console.log(err, 'Register SQL Check error');
+            return next("Mysql error, check your query");
+
+        }
+        if(rows.length){
+           res.status(420).json([{msg:'Email already exists'}]);
+           return;
+           //console.log("Mysql error, check your query");
+        }
+        else{
+            var data = {
+                name:req.body.first_name,
+                email:req.body.email,
+                password:bcrypt.hashSync(req.body.password, null, null),
+                lname:req.body.last_name,
+                phone:req.body.phone,
+                seller:req.body.seller,
+                license:req.body.license
+            };
+
+            dbConnection.query("INSERT INTO t_user set ? ",data, function(err, rows)
+            {
+
+                if (err)
+                {
+                    console.log(err);
+                    return next("Mysql error, check your query");
+                }
+
+                res.sendStatus(200);
+            });
+        }
+
+    })
+
+};
+
 
 module.exports = {
     getAllUsers: getAllUsers,
@@ -203,5 +264,7 @@ module.exports = {
     getUserToEdit:getUserToEdit,
     updateUserInfo:updateUserInfo,
     deleteUser:deleteUser,
-	getSearchUsersPage:getSearchUsersPage
+	getSearchUsersPage:getSearchUsersPage,
+    getSignupForm:getSignupForm,
+    registerUser:registerUser
 };
--- nodeApp/server.js
+++ nodeApp/server.js
@@ -1,25 +1,58 @@
-var express  = require('express'),
-    path     = require('path'),
+var express = require('express'),
+    path = require('path'),
     bodyParser = require('body-parser'),
     app = express(),
-    expressValidator = require('express-validator'),
     users = require('./modules/users'),
     listings = require('./modules/listings'),
     multer = require('multer'),
     upload = multer({dest: "./public/images/upload_images"}),
     fs = require('fs'),
     passport = require('passport'),
-    db = require('./config/db');
+    db = require('./config/db'),
+    session = require('express-session'),
+    // morgan = require('morgan'),
+    flash = require('connect-flash'),
+    cookieParser = require('cookie-parser'),
+    expressValidator = require('express-validator'),
+    bcrypt = require('bcrypt-nodejs'),
+    fileupload = require('express-fileupload');
 
 
-/*Set EJS template Engine*/
-app.set('views','./views');
-app.set('view engine','ejs');
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+// app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: false })); //support x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false})); //support x-www-form-urlencoded
 app.use(bodyParser.json({}));
 app.use(expressValidator());
+
+/*Set EJS template Engine*/
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+// required for passport
+app.use(session({
+    secret: 'vidyapathaisalwaysrunning',
+    resave: true,
+    saveUninitialized: true
+})); // session secret
+
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+app.use(function (req,res, next) {
+    res.locals.success_message = req.flash('SUcc_message');
+    res.locals.error_message = req.flash('error_messages');
+    next();
+
+})
+
 
 // /*MySql connection*/
 // var connection  = require('express-myconnection'),
@@ -41,30 +74,30 @@ app.use(expressValidator());
 // );
 
 //Get home page
-app.get('/',function(req,res){
-	//req.getConnection(function(err,conn){
+app.get('/', function (req, res) {
+    //req.getConnection(function(err,conn){
 
-		//if (err) return next("Cannot Connect");
+    //if (err) return next("Cannot Connect");
 
-		var query = db.query('SELECT * FROM listings ',function(err,rows){
+    var query = db.query('SELECT * FROM listings ', function (err, rows) {
 
-			if(err){
-				console.log(err);
-				return next("Mysql error, check your query");
-			}
+        if (err) {
+            console.log(err);
+            return next("Mysql error, check your query");
+        }
 
-			res.render('index',{title:"Bay Real Estate",data:rows});
-		});
-	//});
+        res.render('index', {title: "Bay Real Estate", data: rows});
+    });
+    //});
 });
 
-app.get('/fa17g12/login', function(req, res){
-    res.render('login',{title:"Login Page", message:''});
-})
+// app.get('/login', function(req, res){
+//     res.render('login',{title:"Login Page", message:''});
+// })
 
-app.get('/fa17g12/signup', function(req, res){
-    res.render('signup',{title:"SignUp Page", message:''});
-})
+// app.get('/api/signup', function(req, res){
+//     res.render('signup',{title:"SignUp Page", message:''});
+// })
 
 
 //Get pages before router
@@ -80,7 +113,7 @@ var router = express.Router();
 *  we can use this for doing validation,authetication
 *  for every route started with /api
 --------------------------------------------------------*/
-router.use(function(req, res, next) {
+router.use(function (req, res, next) {
     console.log(req.method, req.url);
     next();
 });
@@ -93,6 +126,9 @@ app.post('/search/:user', users.searchUser);
 app.get('/api/user/:user_id', users.getUserToEdit);
 app.put('/api/user/:user_id', users.updateUserInfo);
 app.delete('/api/user/:user_id', users.deleteUser);
+app.get('/api/signup', users.getSignupForm);
+app.post('/api/signup',users.registerUser);
+
 
 //LISTINGS EXTERNAL MODULES
 app.get('/api/listings', listings.getAllListings);
@@ -109,8 +145,8 @@ app.get('/api/listing_description/:listing_id', listings.getDescription);
 app.use('/api', router);
 
 //start Server
-var server = app.listen(17012,function(){
+var server = app.listen(17012, function () {
 
-   console.log("Listening to port %s",server.address().port);
+    console.log("Listening to port %s", server.address().port);
 
 });
