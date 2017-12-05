f3e397277ad24b2606fa8dc6fe4ef5ff59b8ec47
JiawenZhu <zhujiawen519@gmail.com>
2017-12-04 17:27:04 -0800

    quick update

--- nodeApp/config/db.js
+++ nodeApp/config/db.js
@@ -4,9 +4,9 @@ var connection = mysql.createConnection({
     // user     : 'root',
     // password : 'b795fk99sw', //set password if there is one
     // database : 'test', //set DB name here
-    user     : 'root',
-    password : '5191', //set password if there is one
-    database : 'test', //set DB name here
+    user     : 'git',
+    password : 'csc648fa17g12', //set password if there is one
+    database : 'fa17g12', //set DB name here
     debug    : false //set true if you wanna see debug logger
 });
 
